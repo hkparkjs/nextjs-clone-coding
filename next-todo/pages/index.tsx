035554ff127d631debd9be689a497e3f9aa27a79
Hyeon Park <hkparkjs@gmail.com>
2023-02-09 21:45:38 +0900

    🎉 Add Settings for next-todo project

---
+++ next-todo/pages/index.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from 'next/types';
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const index: NextPage = () => {
+  return (
+    <Container>
+      <h1>hello styled-components</h1>
+      <h2>hello styled-components</h2>
+      <p>hello styled-components</p>
+      <ul>
+        <li>hello styled-components</li>
+      </ul>
+      <a>hello styled-components</a>
+      <span>hello styled-components</span>
+    </Container>
+  );
+};
+
+export default index;
