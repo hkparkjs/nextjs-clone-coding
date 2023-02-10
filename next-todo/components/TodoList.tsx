c9495fa6e6085ece7c9b6b1a1dafb0705d83b467
Hyeon Park <hkparkjs@gmail.com>
2023-02-10 10:17:20 +0900

    🚧 Add counting the number of todo items by color

--- next-todo/components/TodoList.tsx
+++ next-todo/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useCallback } from 'react';
 import styled from 'styled-components';
 import { TodoType } from '../types/todo';
 import palette from '../styles/palette';
@@ -24,6 +24,68 @@ interface IProps {
 }
 
 const TodoList: React.FC<IProps> = ({ todos }) => {
+  // useCallback : 함수에 종속성을 줌
+  const getTodoColorNums = useCallback(
+    () => {
+      let red = 0;
+      let orange = 0;
+      let yellow = 0;
+      let green = 0;
+      let blue = 0;
+      let navy = 0;
+      todos.forEach((todo) => {
+        if (todo.color === "red") {
+          red += 1;
+        }
+        if (todo.color === "orange") {
+          orange += 1;
+        }
+        if (todo.color === "yellow") {
+          yellow += 1;
+        }
+        if (todo.color === "green") {
+          green += 1;
+        }
+        if (todo.color === "blue") {
+          blue += 1;
+        }
+        if (todo.color === "navy") {
+          navy += 1;
+        }
+      });
+      return {
+        red,
+        orange,
+        yellow,
+        green,
+        blue,
+        navy,
+      }
+    },
+    [todos],
+  );
+  // useMemo : 변수에 종속성을 주어 함수의 재연산 방지
+  const todoColorNums = useMemo(getTodoColorNums, [todos]);
+
+  // 객체의 문자열 인덱스 사용을 위한 타입
+  type ObjectIndexType = {
+    [key: string]: number | undefined;
+  };
+
+  // 색깔 객체 구하기 2
+  const todoColorNums2 = useMemo(() => {
+    const colors: ObjectIndexType = {};
+    todos.forEach((todo) => {
+      const value = colors[todo.color];
+      if (!value) {
+        colors[`${todo.color}`] = 1;
+      } else {
+        colors[`${todo.color}`] = value + 1;
+      }
+    });
+    return colors;
+  }, [todos]);
+
   return (
     <Container>
       <div className="todo-list-header">
