5da73d0ebaad380cadd32e3bd2463a0caf1375ed
Hyeon Park <hkparkjs@gmail.com>
2023-02-13 16:18:48 +0900

    ✨ Add Redux and connect the data

--- next-todo/pages/index.tsx
+++ next-todo/pages/index.tsx
@@ -5,6 +5,8 @@ import TodoList from '../components/TodoList';
 import { TodoType } from '../types/todo';
 import Axios from 'axios';
 import { getTodosAPI } from '../lib/api/todo';
+import { wrapper } from '../store';
+import { todoActions } from '../store/todo';
 
 const Container = styled.div`
   padding: 20px;
@@ -18,14 +20,18 @@ const app: NextPage<IProps> = ({ todos }) => {
   return <TodoList todos={todos} />;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const { data } = await getTodosAPI();
-    return { props: { todos: data } };
-  } catch (e) {
-    console.log(e);
-    return { props: {} };
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    console.log(store);
+    try {
+      const { data } = await getTodosAPI();
+      store.dispatch(todoActions.setTodo(data));
+      return { props: { todos: data } };
+    } catch (e) {
+      console.log(e);
+      return { props: { todos : [] } };
+    }
   }
-};
+);
 
 export default app;
