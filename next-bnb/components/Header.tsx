3277f6e269fa26c84867ef73d07b550b0d7f69cd
Hyeon Park <hkparkjs@gmail.com>
2023-02-15 21:05:02 +0900

    ✨ Add Modal Component

--- next-bnb/components/Header.tsx
+++ next-bnb/components/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import AirbnbLogoIcon from '../public/static/svg/logo/logo.svg';
 import AirbnbLogoTextIcon from '../public/static/svg/logo/logo_text.svg';
 import Link from 'next/link';
 import palette from '../styles/palette';
+import ModalPortal from './ModalPortal';
+import SignUpModal from './auth/SignUpModal';
 
 const Container = styled.div`
   position: sticky;
@@ -53,9 +55,34 @@ const Container = styled.div`
       }
     }
   }
+  .modal-wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    .modal-background {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.75);
+      z-index: 10;
+    }
+    .modal-contents {
+      width: 400px;
+      height: 400px;
+      background-color: white;
+      z-index: 11;
+    }
+  }
 `;
 
 const Header: React.FC = () => {
+  const [modalOpened, setModalOpened] = useState(false);
+
   return (
     <Container>
       <Link href="/">
@@ -65,13 +92,22 @@ const Header: React.FC = () => {
       </div>
       </Link>
       <div className="header-auth-buttons">
-        <button type="button" className="header-sign-up-button">
+        <button
+          type="button"
+          className="header-sign-up-button"
+          onClick={() => setModalOpened(true)}
+        >
           회원가입
         </button>
         <button type="button" className="header-login-button">
           로그인
         </button>
       </div>
+      {modalOpened && (
+        <ModalPortal closePortal={() => setModalOpened(false)}>
+          <SignUpModal />
+        </ModalPortal>
+      )}
     </Container>
   );
 };
