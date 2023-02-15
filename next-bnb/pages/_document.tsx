b5ee04053d4b3e7ecdc4b50af1dcfc7934bb085f
Hyeon Park <hkparkjs@gmail.com>
2023-02-15 12:57:34 +0900

    🔧 Add configuration for Global Style

--- next-bnb/pages/_document.tsx
+++ next-bnb/pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { DocumentContext } from 'next/document';
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import Document from 'next/document';
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -27,5 +34,21 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
