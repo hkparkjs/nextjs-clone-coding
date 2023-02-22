148d033d008d90c08179d769317addb1f8b423d3
Hyeon Park <hkparkjs@gmail.com>
2023-02-22 21:47:57 +0900

    ✨ Add Login API

--- next-bnb/components/auth/LoginModal.tsx
+++ next-bnb/components/auth/LoginModal.tsx
@@ -9,6 +9,7 @@ import Button from '../common/Button';
 import Input from '../common/Input';
 import { useDispatch } from 'react-redux';
 import { authActions } from '../../store/auth';
+import { loginAPI } from '../../lib/api/auth';
 
 const Container = styled.form`
   width: 568px;
@@ -75,8 +76,26 @@ const LoginModal: React.FC<IProps> = ({ closeModal }) => {
     dispatch(authActions.setAuthMode("signup"));
   };
 
+  //* 로그인 클릭 시
+  const onSubmitLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!email || !password) {
+      alert("이메일과 비밀번호를 입력해주세요.");
+    } else {
+      const loginBody = { email, password };
+
+      try {
+        const { data } = await loginAPI(loginBody);
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
   return (
-    <Container>
+    <Container onSubmit={onSubmitLogin}>
       <CloseXIcon className="modal-close-x-icon" onClick={closeModal} />
       <div className="login-input-wrapper">
         <Input
