97e78bf68e759b080034cfed4014e637dd6af738
Hyeon Park <hkparkjs@gmail.com>
2023-02-26 21:49:09 +0900

    ✨ Add Radio Component for selecting Building Type

--- next-bnb/store/registerRoom.ts
+++ next-bnb/store/registerRoom.ts
@@ -1,12 +1,10 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { useSeletor } from '../../../store';
-
 
 type RegisterRoomState = {
   largeBuildingType: string | null;
   buildingType: string | null;
   roomType: string | null;
-  isSetupForGuest: boolean | null;
+  isSetUpForGuest: boolean | null;
 };
 
 //* 초기 상태
@@ -18,7 +16,7 @@ const initialState: RegisterRoomState = {
   //* 숙소 유형
   roomType: null,
   //* 게스트만을 위해 만들어진 숙소인가
-  isSetupForGuest: null
+  isSetUpForGuest: null
 };
 
 const registerRoom = createSlice({
@@ -37,6 +35,15 @@ const registerRoom = createSlice({
         state.buildingType = null;
       }
       state.buildingType = action.payload;
+      return state;
+    },
+    setRoomType(state, action: PayloadAction<"entire" | "private" | "public">) {
+      state.roomType = action.payload;
+      return state;
+    },
+    //* '게스트용 숙소인지' 변경하기
+    setIsSetUpForGuest(state, action: PayloadAction<boolean>) {
+      state.isSetUpForGuest = action.payload;
       return state;
     }
   },
