c1079b7c20a29d5e066520b6ec728ec46fb7d22c
Hyeon Park <hkparkjs@gmail.com>
2023-02-27 21:00:51 +0900

    ✨ Add Counter Component and UI for counting the number of bedroom and bed

--- next-bnb/store/registerRoom.ts
+++ next-bnb/store/registerRoom.ts
@@ -1,10 +1,16 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { BedType } from '../types/room';
 
 type RegisterRoomState = {
   largeBuildingType: string | null;
   buildingType: string | null;
   roomType: string | null;
   isSetUpForGuest: boolean | null;
+  maximumGuestCount: number;
+  bedroomCount: number;
+  bedCount: number;
+  bedList: { id: number; beds: { type: BedType; count: number }[] }[];
+  publicBedList: { type: BedType; count: number }[];
 };
 
 //* 초기 상태
@@ -16,7 +22,17 @@ const initialState: RegisterRoomState = {
   //* 숙소 유형
   roomType: null,
   //* 게스트만을 위해 만들어진 숙소인가
-  isSetUpForGuest: null
+  isSetUpForGuest: null,
+  //* 최대 숙박 인원
+  maximumGuestCount: 1,
+  //* 침실 개수
+  bedroomCount: 0,
+  //* 침대 개수
+  bedCount: 1,
+  //* 침대 유형
+  bedList: [],
+  //* 공용공간 침대 유형
+  publicBedList: [],
 };
 
 const registerRoom = createSlice({
@@ -45,7 +61,22 @@ const registerRoom = createSlice({
     setIsSetUpForGuest(state, action: PayloadAction<boolean>) {
       state.isSetUpForGuest = action.payload;
       return state;
-    }
+    },
+    //* 최대 숙박 인원 변경하기
+    setMaximumGuestCount(state, action: PayloadAction<number>) {
+      state.maximumGuestCount = action.payload;
+      return state;
+    },
+    //* 침실 개수 변경하기
+    setBedroomCount(state, action: PayloadAction<number>) {
+      state.bedroomCount = action.payload;
+      return state;
+    },
+    //* 최대 침대 개수 변경하기
+    setBedCount(state, action: PayloadAction<number>) {
+      state.bedCount = action.payload;
+      return state;
+    },
   },
 });
 
