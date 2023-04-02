e34c8a2a92455c7b2a0cfb12f57fa423fa167050
Hyeon Park <hkparkjs@gmail.com>
2023-04-02 16:00:47 +0900

    ✨ Add Guests Input

--- next-bnb/components/common/Button.tsx
+++ next-bnb/components/common/Button.tsx
@@ -31,6 +31,11 @@ const getButtonColor = (color: string, colorReverse: boolean) => {
         background-color: ${palette.bittersweet};
         color: white;
       `;
+    case "amaranth":
+      return css`
+        background-color: ${palette.amaranth};
+        color: white;
+      `;
     default:
       return css`
         background-color: white;
@@ -88,7 +93,7 @@ const Container = styled.button<StyledButtonProps>`
 
 interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  color?: "dark_cyan" | "bittersweet" | "white";
+  color?: "dark_cyan" | "bittersweet" | "white" | "amaranth";
   width?: string;
   colorReverse?: boolean;
   icon?: JSX.Element;
